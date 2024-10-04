93d558c3c622787a803130edc6cbf7f2111e1821
zaram <echukwuzaram@gmail.com>
2024-10-04 22:28:29 +0100

    first commit

---
+++ madlibs.py
@@ -0,0 +1,20 @@
+#string concatenation
+#youtuber = "zedthelguth" #some string variable
+
+#a few ways to concatenate are 
+#print("subscribe to " + youtuber)
+#print(f"subscribe to {youtuber}")
+
+adj = input("Adjective:")
+adj2 = input("Adjective:")
+noun =  input("Noun:")
+pronoun1 = input("Pronoun:")
+pronoun2 = input("Third Person Pronoun:")
+
+
+
+madlibs = f"Reading manga is so {adj}!.Whenever i read a manga it makes me feel {adj2}. My best manga charachter is {noun}!\
+    {pronoun1} inspires me to do things that {pronoun1} did! i love {pronoun2} so much"
+
+
+print(madlibs)
